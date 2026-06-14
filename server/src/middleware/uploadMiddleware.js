6f7935791a4cbb92bed4606dcbc9fd41a58272b4
Hadi Shah <hadishah.work@gmail.com>
2026-06-15 01:40:02 +0530

    fix resume upload directory for production

---
+++ server/src/middleware/uploadMiddleware.js
@@ -0,0 +1,42 @@
+import multer from "multer";
+import fs from "fs";
+
+if (
+  !fs.existsSync(
+    "uploads"
+  )
+) {
+  fs.mkdirSync(
+    "uploads"
+  );
+}
+
+const storage =
+  multer.diskStorage({
+    destination(
+      req,
+      file,
+      cb
+    ) {
+      cb(
+        null,
+        "uploads/"
+      );
+    },
+
+    filename(
+      req,
+      file,
+      cb
+    ) {
+      cb(
+        null,
+        `${Date.now()}-${file.originalname}`
+      );
+    },
+  });
+
+export const upload =
+  multer({
+    storage,
+  });
